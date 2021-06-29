5291495260e2f2305fb45adb4e5696e1f5662dc7
LeeEunBi <eblee@weedscomm.com>
2021-06-29 15:41:34 +0900

    서버 구현 수정 > IP 주소 표시 > 연결된 Client 에 접근

--- app/src/main/java/com/example/tcpip_server/MainActivity.kt
+++ app/src/main/java/com/example/tcpip_server/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.tcpip_server
 
+import android.annotation.SuppressLint
+import android.net.wifi.WifiManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var serverIP: TextView
+    lateinit var btnDevice1: Button
+    lateinit var btnDevice2: Button
+
+    var server = SocketClient()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        serverIP = findViewById(R.id.tv_server_ip)
+        btnDevice1 =  findViewById(R.id.btn_device1)
+        btnDevice1.setOnClickListener(this)
+        btnDevice2 = findViewById(R.id.btn_device2)
+        btnDevice2.setOnClickListener(this)
+
+        try {
+            val ip = getLocalIpAddress()
+            serverIP.text = ip.toString()
+        } catch (e: UnknownHostException) {
+            e.printStackTrace()
+        }
+
+        Thread {
+            server.start()
+        }.start()
     }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_device1 -> {
+                server.sendDevice1("디바이스 1 응답하라")
+            }
+            R.id.btn_device2 -> {
+                server.sendDevice2("디바이스 2 응답하라")
+            }
+        }
+    }
+
+
+    @SuppressLint("ServiceCast")
+    @Throws(UnknownHostException::class)
+    private fun getLocalIpAddress(): String? {
+
+        val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        val wifiInfo = wifiManager.connectionInfo
+        val ipInt = wifiInfo.ipAddress
+
+        return InetAddress.getByAddress(
+            ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(ipInt).array()
+        )
+            .hostAddress
+    }
+
 }
--- app/src/main/java/com/example/tcpip_server/SocketClient.kt
+++ app/src/main/java/com/example/tcpip_server/SocketClient.kt
@@ -1,6 +1,8 @@
 package com.example.tcpip_server
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.IOException
@@ -9,20 +11,24 @@ import java.net.Socket
 import java.util.*
 import kotlin.collections.HashMap
 
-class Server {
+
+fun main() {
+    SocketClient().start()
+}
+
+class SocketClient {
 
     var clients: HashMap<String, DataOutputStream> = HashMap()
+    var device1: DataOutputStream? = null
+    var device2: DataOutputStream? = null
+
     var serverSocket: ServerSocket? = null
 
     init {
         Collections.synchronizedMap(clients)
     }
 
-    fun main(args: Array<String>) {
-        start()
-    }
-
-    private fun start() {
+    fun start() {
         val port = 5001
         var socket: Socket? = null
 
@@ -30,13 +36,16 @@ class Server {
             // 서버 소켓 생성 > accept(대기) > 접속 시 ip주소 획득 > 출력
             // MultiThread 생성
             serverSocket = ServerSocket(port)
-            Log.d("Start >> ", "접속 대기중")
+            Log.d("Server >>", "접속 대기중")
+
             while (true) {
                 if (serverSocket != null) {
                     socket = serverSocket!!.accept()
                     val ip = socket.inetAddress // ip 주소 획득
-                    print("ip 주소 >> $ip connected")
-
+                    Log.d("ip 주소 >> ", ip.toString())
+                    MultiThread(socket).start()
+                } else {
+                    print("socket null")
                 }
             }
         } catch (e: IOException) {
@@ -44,12 +53,36 @@ class Server {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun sendDevice1(msg: String) {
+        if (device1 != null) {
+            Thread {
+                try {
+                    val output = DataOutputStream(device1)
+                    output.writeUTF(msg)
+                } catch (e: IOException) {
+
+                }
+            }.start()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun sendDevice2(msg: String) {
+        if (device2 != null) {
+            Thread {
+                try {
+                    val output = DataOutputStream(device2)
+                    output.writeUTF(msg)
+                } catch (e: IOException) {
+                }
+            }.start()
+        }
+    }
+
 
     inner class MultiThread constructor(_socket: Socket) : Thread() {
         var socket: Socket? = null
-        var mac: String? = null
-        var msg: String? = null
-
         var input: DataInputStream? = null
         var output: DataOutputStream? = null
 
@@ -59,17 +92,25 @@ class Server {
                 input = DataInputStream(_socket.getInputStream())
                 output = DataOutputStream(_socket.getOutputStream())
             } catch (e: IOException) {
+
             }
         }
 
         override fun run() {
             try {
                 // mac 주소를 받아옴 > 출력
-                mac = input?.readUTF()
-                print("mac 주소 >>> $mac")
+//                mac = input?.readUTF()
+                val nickName = input?.readUTF()
+                val ip = socket!!.inetAddress // ip 주소 획득
 
-                clients.put(mac!!, output!!)
-                sendMsg("$mac > 접속")
+                clients.put(nickName ?: "", output!!)
+                if (device1 == null) {
+                    device1 = output!!
+                } else {
+                    device2 = output!!
+                }
+
+                sendMsg("$nickName 님이 접속했습니다.")
 
                 while (input != null) {
                     try {
@@ -82,25 +123,22 @@ class Server {
                     }
                 }
             } catch (e: IOException) {
+
             }
         }
 
 
         private fun sendMsg(msg: String) {
-
             val it: Iterator<String> = clients.keys.iterator()
 
             while (it.hasNext()) {
                 try {
                     val dos: DataOutputStream = clients[it.next()]!!
-
                     output = DataOutputStream(dos)
                     output!!.writeUTF(msg)
-
                 } catch (e: IOException) {
                 }
             }
-
         }
     }
 }
